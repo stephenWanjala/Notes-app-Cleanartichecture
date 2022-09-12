3d98a403ea27519a285578cd7abf77d401816c28
stephen wanjala <stephenwanjala145@gmail.com>
2022-09-12 19:35:09 +0300

    ordersection +fab on Note Screen

--- app/src/main/java/com/wantech/notes/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/wantech/notes/feature_note/presentation/MainActivity.kt
@@ -3,17 +3,21 @@ package com.wantech.notes.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
 import com.wantech.notes.feature_note.domain.util.NoteOrder
+import com.wantech.notes.feature_note.presentation.note.components.NotesScreen
 import com.wantech.notes.feature_note.presentation.note.components.OrderSection
 import com.wantech.notes.ui.theme.NotesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesTheme {
-
+                val navController = rememberNavController()
+                NotesScreen(navController = navController )
             }
         }
     }
--- app/src/main/java/com/wantech/notes/feature_note/presentation/note/components/NotesScreen.kt
+++ app/src/main/java/com/wantech/notes/feature_note/presentation/note/components/NotesScreen.kt
@@ -1,12 +1,18 @@
 package com.wantech.notes.feature_note.presentation.note.components
 
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Sort
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.wantech.notes.feature_note.presentation.note.NotesEVent
 import com.wantech.notes.feature_note.presentation.note.NotesViewModel
 
 @Composable
@@ -14,17 +20,49 @@ fun NotesScreen(
     navController: NavController,
     viewModel: NotesViewModel = hiltViewModel(),
 
-) {
-    val state =viewModel.state
+    ) {
+    val state = viewModel.state
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
-    
-    Scaffold(floatingActionButton = { 
-        FloatingActionButton(onClick = { /*TODO*/ }) {
-            
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add note"
+                )
+            }
+        },
+        scaffoldState = scaffoldState
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+
+                ) {
+                Text(
+                    text = "Your Notes here",
+                    style = MaterialTheme.typography.h4
+                )
+                IconButton(onClick = {
+                    viewModel.onEvent(NotesEVent.ToggleOrderSection)
+                }) {
+                    Icon(imageVector = Icons.Default.Sort, contentDescription = "Icon Sort")
+                }
+            }
         }
-    }) {
-        
+
     }
 
 }
