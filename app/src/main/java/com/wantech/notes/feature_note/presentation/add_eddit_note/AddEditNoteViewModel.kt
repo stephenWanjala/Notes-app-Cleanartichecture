3bcccdbea553028f8bc31735c81f9406dba3eaa9
stephen wanjala <stephenwanjala145@gmail.com>
2022-09-12 20:44:43 +0300

    add/edit note viewModel

---
+++ app/src/main/java/com/wantech/notes/feature_note/presentation/add_eddit_note/AddEditNoteViewModel.kt
@@ -0,0 +1,41 @@
+package com.wantech.notes.feature_note.presentation.add_eddit_note
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.toArgb
+import androidx.lifecycle.ViewModel
+import com.wantech.notes.feature_note.domain.model.Note
+import com.wantech.notes.feature_note.domain.usecase.NotesUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class AddEditNoteViewModel @Inject constructor(
+    private val notesUseCases: NotesUseCases
+) : ViewModel() {
+    private var _noteTittle = mutableStateOf<NoteTextFieldState>(
+        NoteTextFieldState(
+            hint = "Enter Tittle"
+        )
+    )
+    val noteTittle: State<NoteTextFieldState> = _noteTittle
+
+    private var _noteContent = mutableStateOf<NoteTextFieldState>(
+        NoteTextFieldState(
+            hint = "Enter some Content"
+        )
+    )
+    val noteContent: State<NoteTextFieldState> = _noteContent
+    private var _noteColor = mutableStateOf<Int>(Note.noteColors.random().toArgb())
+    val noteColor: State<Int> = _noteColor
+
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+
+    sealed class UiEvent {
+        data class ShowSnackBar(val message: String) : UiEvent()
+        object SaveNote : UiEvent()
+    }
+
+
+}
---
+++ app/src/main/java/com/wantech/notes/feature_note/presentation/add_eddit_note/NoteTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.wantech.notes.feature_note.presentation.add_eddit_note
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
