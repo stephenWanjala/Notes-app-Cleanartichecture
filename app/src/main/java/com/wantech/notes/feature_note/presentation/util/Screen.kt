5aa0c0fa571979529ea5f409a9ae0ff13ee65a9a
stephen wanjala <stephenwanjala145@gmail.com>
2022-09-12 23:23:52 +0300

    screens/posible destinations

---
+++ app/src/main/java/com/wantech/notes/feature_note/presentation/util/Screen.kt
@@ -0,0 +1,6 @@
+package com.wantech.notes.feature_note.presentation.util
+
+sealed class Screen(val route:String){
+    object  NotesScreen:Screen("notes_Screen")
+    object AddEditNoteScreen:Screen("add_edit_note_screen")
+}
